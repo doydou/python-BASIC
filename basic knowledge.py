cdc4756a27df91d886af53101085cb7010579841
双宁 <972126586@qq.com>
2020-07-29 18:32:50 +0800

    补充提交

--- basic knowledge.py
+++ basic knowledge.py
@@ -8,38 +8,53 @@
 使用int()进行类型转换
 用占位符格式化输出的字符串
 """
-a = int(input('a = '))
-b = int(input('b = '))
-print('%d + %d = %d' % (a, b, a + b))
-print('%d - %d = %d' % (a, b, a - b))
-print('%d * %d = %d' % (a, b, a * b))
-print('%d / %d = %f' % (a, b, a / b))
-print('%d // %d = %d' % (a, b, a // b))
-print('%d %% %d = %d' % (a, b, a % b))
-print('%d ** %d = %d' % (a, b, a ** b))
+# a = int(input('a = '))
+# b = int(input('b = '))
+# print('%d + %d = %d' % (a, b, a + b))
+# print('%d - %d = %d' % (a, b, a - b))
+# print('%d * %d = %d' % (a, b, a * b))
+# print('%d / %d = %f' % (a, b, a / b))
+# print('%d // %d = %d' % (a, b, a // b))
+# print('%d %% %d = %d' % (a, b, a % b))
+# print('%d ** %d = %d' % (a, b, a ** b))
 
 """
 使用type()检查变量的类型
 """
-a = 100
-b = 12.345
-c = 1 + 5j
-d = 'hello, world'
-e = True
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))
-print(type(e))
+# a = 100
+# b = 12.345
+# c = 1 + 5j
+# d = 'hello, world'
+# e = True
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
 
 ###练习
 """
-将华氏温度转换为摄氏温度
-F = 1.8C + 32
+1。计算并打印出 993 + 196 的和， 乘以 7 的积， 然后除以3
+2.计算并打印出 993 + 196 的和，乘以 7 的积， 然后除以3 的 商 和 余数
+3.将华氏温度转换为摄氏温度（F = 1.8C + 32）
+4.输入半径计算圆的周长和面积
+5.输入年份，判断是否是闰年
 """
+# f = float(input("请输入华式温度为："))
+# c = (f-32)/1.8
+# print("%.1f华式度=%.1f摄氏度"%(f,c))
 
-#1. 计算并打印出 993 + 196 的和， 乘以 7 的积， 然后除以3
-#2.计算并打印出 993 + 196 的和，乘以 7 的积， 然后除以3 的 商 和 余数
-print((993 + 136)*7/3)#"/"是除号
-print((993+136)*7//3)#商
-print((993+196)*7%3)#余数
+
+import math
+radius = float(input("请输入半径："))
+perimeter = 2*math.pi*radius
+area = math.pi*radius*radius
+print("园的周长为{:.2}，面积为{:.2}".format(perimeter,area))
+
+
+
+
+
+# print((993 + 136)*7/3)#"/"是除号
+# print((993+136)*7//3)#商
+# print((993+196)*7%3)#余数
