e6541c93171b80ecad57c1fb0115c9bd8791e7d5
双宁 <972126586@qq.com>
2020-08-12 17:35:30 +0800

    完善字典部分知识

--- Love PYTHON/4.字典.py
+++ Love PYTHON/4.字典.py
@@ -10,4 +10,19 @@ popitem()	        随机返回并删除字典中的一对键和值
 dict.items()	    以列表返回可遍历的(键, 值) 元组数组
 '''
 #创建字典
-dict = {1:'豆豆',}
+dict1 = {1:'豆豆',2:'胖胖',3:'nene'}
+#通过键可以获取字典中对应的值
+print(dict1[1])
+#对字典进行遍历
+for i in dict1:
+    print('%d--->%s' % (i,dict1[i]))
+#对字典更新元素
+dict1[1] = '糕糕'
+dict2 = {4:'粤粤'}
+dict1.update(dict2)
+print(dict1)
+#通过键获取对应的值
+print(dict1.get(1))
+#删除字典中的元素
+print(dict1.pop(2))
+print(dict1)
