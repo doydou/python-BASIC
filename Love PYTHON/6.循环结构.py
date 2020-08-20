c0b3b06b2cfc1a827dd4231c0ce7c2ddb27f1343
双宁 <972126586@qq.com>
2020-08-20 15:48:37 +0800

    提交

--- Love PYTHON/6.循环结构.py
+++ Love PYTHON/6.循环结构.py
@@ -1,6 +1,8 @@
-'''for-in循环
+'''
+for-in循环
 如果明确的知道循环执行的次数或者是要对一个容器进行迭代
-那么我们推荐使用for-in循环'''
+那么我们推荐使用for-in循环
+'''
 """
 用for循环实现1~100求和
 """
@@ -23,24 +25,24 @@ While循环
 计算机出一个1~100之间的随机数由人来猜
 计算机根据人猜的数字分别给出提示大一点/小一点/猜对了
 """
-# import random
-# answer = random.randint(1,100)
-# times = 0
-# while True:
-#     times = times + 1
-#     num = int(input("请输入："))
-#     if num < answer:
-#         print("小了")
-#     elif num > answer:
-#         print("大了")
-#     else:
-#         print("恭喜你猜对了")
-#         break
-# print("你一共猜了%d次"%times)
-# if times > 7:
-#     print("你太笨了")
-# else:
-#     print("你太厉害了")
+import random
+answer = random.randint(1,100)
+times = 0
+while True:
+    times = times + 1
+    num = int(input("请输入："))
+    if num < answer:
+        print("小了")
+    elif num > answer:
+        print("大了")
+    else:
+        print("恭喜你猜对了")
+        break
+print("你一共猜了%d次"%times)
+if times > 7:
+    print("你太笨了")
+else:
+    print("你太厉害了")
 
 #练习：1输出乘法口诀表(九九表)
 for i in range(1,10):
