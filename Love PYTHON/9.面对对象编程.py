8146de325ec740393718f4ca1bcb9d2514d01a58
双宁 <972126586@qq.com>
2020-08-21 18:34:46 +0800

    提交

--- Love PYTHON/9.面对对象编程.py
+++ Love PYTHON/9.面对对象编程.py
@@ -4,36 +4,62 @@
 在面向对象编程的世界中，一切皆为对象，对象都有属性和行为，每个对象都是独一无二的，而且对象一定属于某个类（型）。
 当我们把一大堆拥有共同特征的对象的静态特征（属性）和动态特征（行为）都抽取出来后，就可以定义出一个叫做“类”的东西。
 '''
-#创建和使用类
-class Student(object):
+##使用类和实例--创建car类
+class Car():
+    def __init__(self,make,model,year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_num = 0 #给属性指定默认的值
 
-    #__init__是一个特殊方法用于在创建对象时进行初始化操作
-    #通过这个方法我们可以为学生对象绑定name和age两个属性
-    def __init__(self,name,age):
-        self.name = name
-        self.age = age
+    def get_descriptive_name(self):
+        """返回整洁的描述性信息"""
+        full_name = str(self.year) + ' ' + self.model + ' ' + self.make
+        return full_name
 
-    def study(self,course_name):
-        print('%s正在学习%s' % (self.name,course_name))
-    #标识符全小写用下划线连接
-    def watch_film(self):
-        if self.age < 18:
-            print('%s只能看小电影' % self.name)
-        else:
-            print('%s可以看动作片' % self.name)
+    def read_odometer(self):
+        print('this car has ' + str(self.odometer_num) + ' miles on road')
 
-#创建和使用对象
-def main():
-    #创建学生对象
-    student1 = Student('糕糕',23)
-    #给对象发study消息
-    student1.study('math')
-    #给对象发watch——film消息
-    student1.watch_film()
+    """2.方法
+    对car类添加一个新的方法
+    """
+    def update_odometer(self,mileage):
+        """将里程数设置为指定的值"""
+        self.odometer_num = mileage
+    """3.方法
+    对属性的值进行递增
+    """
+    def increment_odometer(self,miles):
+        self.odometer_num += miles
 
+my_new_car = Car('audi', 'a4', 2016)
+print(my_new_car.get_descriptive_name())
 
-if __name__ == '__main__':
-    main()
+#修改属性的值
+#1.直接修改属性的值
+my_new_car.odometer_num = 33
+my_new_car.read_odometer()
+#2.通过方法修改属性的值
+my_new_car.update_odometer(23)
+my_new_car.read_odometer()
+#3.通过方法对属性的值进行递增
+my_new_car.increment_odometer(10)
+my_new_car.read_odometer()
+
+'''
+继承——一个类继继承承 另一个类时，它将自动获得另一个类的所有属性和方法；原有的 类称为父父类类 ，而新类称为子子类类 。
+子类继承了其父类的所有属性和方法，同时还可以定义自己的属性和方法。
+'''
+#创建子类
+class ElectricCar(Car):
+
+    def __init__(self,make,model,year):#初始化父类的属性
+        #调用父类
+        super().__init__(make,model,year)
+
+my_Tesla = ElectricCar('tesla',"model's",2020)
+print(my_Tesla.get_descriptive_name())
+
 
 
 #练习1：定义一个类描述数字时钟
