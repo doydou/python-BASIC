80590172c77ab0d4da00e6b32bc433d972f6032e
shuangning <972126586@qq.com>
2020-08-26 00:55:40 +0800

    基础补全提交

--- Alien/logic.py
+++ Alien/logic.py
@@ -1,25 +1,30 @@
 import sys#使用sys模块来退出游戏
 import pygame
-import settings
+from settings import Settings
+from ship import Ship
 
 
 def run_game():
     pygame.init()#初始化背景设置
-    screen = pygame.display.set_mode((1200,800)) #设置屏幕大小
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height)) #设置屏幕大小
     pygame.display.set_caption("Alien Invasion")
 
-    #设置背景色
-    bg_color = (230,230,230)
+    #创建一艘飞创
+    ship = Ship(screen)
+
     #开始游戏的主循环
     while True:
 
         #监视键盘和鼠标时间
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        # for event in pygame.event.get():
+        #     if event.type == pygame.QUIT:
+        #         sys.exit()
 
         #每次循环时都重绘屏幕
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+
         #让屏幕可见
         pygame.display.flip()
 
---
+++ Alien/ship.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Ship():
+
+    def __init__(self,screen):
+        '''初始化飞船及其位置'''
+        self.screen = screen
+
+        #加载飞船图像并获取其外形
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()#获取相应的surface的属性
+        self.screen_rect = screen.get_rect()
+
+        #将每艘飞船放在屏幕底部中央
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def blitme(self):
+        #在指定位置上绘制飞创
+        self.screen.blit(self.image,self.rect)
