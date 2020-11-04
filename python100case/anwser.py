42cd963aac1d689eaf06a61bf022a12c17351972
双宁 <972126586@qq.com>
2020-11-04 17:53:49 +0800

    习题添加

--- python100case/anwser.py
+++ python100case/anwser.py
@@ -17,4 +17,31 @@ for i in range(1,5):
 60万到100万之间时，高于60万元的部分，可提成1.5%，
 高于100万元时，超过100万元的部分按1%提成，从键盘输入当月利润I，求应发放奖金总数？
 程序分析：请利用数轴来分界，定位。
+
+while True:
+    profit = int(input('the profit is：'))
+    print("利润是%d万元" % profit)
+    bonus = 0
+    if profit <= 10:
+        bonus = profit*0.1
+    elif profit <= 20 and profit > 10:
+        bonus = profit*0.075
+    elif profit >= 20 and profit < 40:
+            bonus = 10 * 0.1 + (20 - 10) * 0.075 + 0.05 * (profit - 20)
+    elif profit >= 40 and profit < 60:
+            bonus = 10 * 0.1 + (20 - 10) * 0.075 + 0.05 * (40 - 20) + 0.03 * (profit - 40)
+    elif profit >= 60 and profit < 100:
+            bonus= 10 * 0.1 + (20 - 10) * 0.075 + 0.05 * (40 - 20) + 0.03 * (60 - 40) +\
+                   (100 - 60) * 0.015 +(profit - 100) * 0.01
+    else:
+        bonus = 10 * 0.1 + (20 - 10) * 0.075 + 0.05 * (40 - 20) + 0.03 * (60 - 40) + (100 - 60) * 0.015 +\
+                (profit - 100) * 0.01
+    print("奖金是 %f 万元" % bonus)
 '''
+
+#上面的方法比较笨。下面为更简洁高效的方案
+#从上面代码我们可以发现，上面利润的计算方法有一个通式
+#就是（a-b）*c————如：10 * 0.1=（10-0）*0.1
+money = int(input('the profit is：'))
+
+    
