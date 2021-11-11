36c003887871f791bf394ef68a3f383ddb5081b5
宁 双 <972126586@qq.com>
2021-11-11 15:28:31 +0800

    button补充提交

--- Alien/game_stats.py
+++ Alien/game_stats.py
@@ -1,10 +1,13 @@
+class GameStats():
+    """跟踪游戏的统计信息"""
+
 #添加一个play按钮
-def __init__(self, ai_settings):
-    """初始化统计信息"""
-    self.ai_settings = ai_settings
-    self.reset_stats()
+    def __init__(self, ai_settings):
+        """初始化统计信息"""
+        self.ai_settings = ai_settings
+        self.reset_stats()
 
     #让游戏一开始处于非活动状态
-    self.game_active = False
+        self.game_active = False
 
-def reset_stats(self):
+    def reset_stats(self):
